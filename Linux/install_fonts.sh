2e23d88b154631c5f371c6dc071317a35a87cc22
Dasun Nethsara <techsaralk.pro@gmail.com>
2025-02-18 22:05:16 +0530

    Add detailed comments to install_fonts.sh for better understanding

--- Linux/install_fonts.sh
+++ Linux/install_fonts.sh
@@ -1,3 +1,13 @@
+# introduce the program
+# This script installs TrueType fonts from a specified directory to the system fonts directory on Linux.
+# The script assumes that the TrueType fonts are stored in a directory named "fonts" in the same directory as the script.
+# The script will copy all TrueType font files with a ".ttf" extension from the "fonts" directory to the system fonts directory (/usr/share/fonts/truetype/).
+# The script will then update the font cache to make the new fonts available to the system.
+# The script requires sudo privileges to copy files to the system fonts directory and update the font cache.
+# The script should be run from the same directory as the "fonts" directory containing the TrueType font files.
+# The script can be run by executing the following command in the terminal: bash install_fonts.sh
+# The script can be modified to specify a different directory for the TrueType fonts by changing the "fonts_dir" variable.
+
 #!/bin/bash
 
 # Path to the fonts directory
